e9b25ba88b6c3d9ff5b9b8d68e98f1a84debe0bf
Krzysztof Leśniak <klesniak@syncad.com>
2025-07-30 16:49:12 +0200

    Drop test db if test is successful

--- run_tests.sh
+++ run_tests.sh
@@ -4,18 +4,19 @@ _psql() {
     psql -w -v ON_ERROR_STOP=1 -h localhost -U haf_admin "$@"
 }
 
-run_test() {(
-    set -e
+run_test() {
     TEST="$1"
     DB_NAME="$2"
     ./haf/scripts/setup_db.sh --haf-db-name="$DB_NAME"
     _psql -d "$DB_NAME" -f tests/setup.sql
     _psql -d "$DB_NAME" -f db/schema.sql -f db/builtin_roles.sql -f db/nft_actions.sql -f db/main_loop.sql
     _psql -d "$DB_NAME" -f "tests/functions.sql" -f "tests/$TEST.sql" -c 'CALL test_given()' -c 'CALL test_when()' -c 'CALL test_then()'
-)}
+}
 
+set -e
 find ./tests/ -type f -iname test-\*.sql | while read -r file; do
     echo "Running $file"
     stem=$(basename "$file" .sql)
     run_test "$stem" "nft_$(echo "$stem" | sed -E s/[^[:alnum:]]+/_/g)"
+    _psql -d postgres -c "DROP DATABASE $DB_NAME"
 done
