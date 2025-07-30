345762b0df58fff5d441f20aec90290f6f2e0b4b
Krzysztof Leśniak <klesniak@syncad.com>
2025-07-30 13:03:57 +0200

    Haf submodule

--- run_tests.sh
+++ run_tests.sh
@@ -8,7 +8,7 @@ run_test() {(
     set -e
     TEST="$1"
     DB_NAME="$2"
-    ../haf/scripts/setup_db.sh --haf-db-name="$DB_NAME"
+    ./haf/scripts/setup_db.sh --haf-db-name="$DB_NAME"
     _psql -d "$DB_NAME" -f tests/setup.sql
     _psql -d "$DB_NAME" -f db/schema.sql -f db/builtin_roles.sql -f db/nft_actions.sql -f db/main_loop.sql
     _psql -d "$DB_NAME" -f "tests/functions.sql" -f "tests/$TEST.sql" -c 'CALL test_given()' -c 'CALL test_when()' -c 'CALL test_then()'
