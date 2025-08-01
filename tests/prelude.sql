b3c782eddd4bc57dd85f447206c0ed4389531540
Krzysztof Leśniak <klesniak@syncad.com>
2025-08-01 14:59:38 +0200

    Add optional pos parameter

--- tests/prelude.sql
+++ tests/prelude.sql
@@ -1,16 +1,16 @@
-CREATE OR REPLACE PROCEDURE insert_custom_json_operation(block_num INT, auth hive.account_name_type, id TEXT, data jsonb)
+CREATE OR REPLACE PROCEDURE insert_custom_json_operation(block_num INT, pos INT, auth hive.account_name_type, id TEXT, data jsonb)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    INSERT INTO hafd.operations VALUES (hafd.operation_id(block_num,18,0), 0, 0, format('{"type":"custom_json_operation","value":{"id":"%s", "json":%s, "required_auths":[], "required_posting_auths":[%s]}}'::text, id, to_jsonb(data::text)::text, to_jsonb(auth)::text)::jsonb::hafd.operation);
+    INSERT INTO hafd.operations VALUES (hafd.operation_id(block_num,18,pos), 0, 0, format('{"type":"custom_json_operation","value":{"id":"%s", "json":%s, "required_auths":[], "required_posting_auths":[%s]}}'::text, id, to_jsonb(data::text)::text, to_jsonb(auth)::text)::jsonb::hafd.operation);
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE insert_nft_register_op(block_num INT, auth hive.account_name_type, name TEXT, symbol nfttracker_app.symbol, owner TEXT, issuers hive.account_name_type[], max_count INT)
+CREATE OR REPLACE PROCEDURE insert_nft_register_op(block_num INT, auth hive.account_name_type, name TEXT, symbol nfttracker_app.symbol, owner TEXT, issuers hive.account_name_type[], max_count INT, pos INT DEFAULT 0)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    CALL insert_custom_json_operation(block_num, auth, 'NFT', format('{"action": "register", "name": %s, "symbol": %s, "owner": %s, "issuers": %s, "max_count": %s}',
+    CALL insert_custom_json_operation(block_num, pos, auth, 'NFT', format('{"action": "register", "name": %s, "symbol": %s, "owner": %s, "issuers": %s, "max_count": %s}',
         to_jsonb(name)::text,
         to_jsonb(symbol)::text,
         to_jsonb(owner)::text,
@@ -20,11 +20,11 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE insert_nft_modify_op(block_num INT, auth hive.account_name_type, name TEXT, symbol nfttracker_app.symbol, owner TEXT, issuers hive.account_name_type[], max_count INT)
+CREATE OR REPLACE PROCEDURE insert_nft_modify_op(block_num INT, auth hive.account_name_type, name TEXT, symbol nfttracker_app.symbol, owner TEXT, issuers hive.account_name_type[], max_count INT, pos INT DEFAULT 0)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    CALL insert_custom_json_operation(block_num, auth, 'NFT', format('{"action": "modify", "name": %s, "symbol": %s, "owner": %s, "issuers": %s, "max_count": %s}',
+    CALL insert_custom_json_operation(block_num, pos, auth, 'NFT', format('{"action": "modify", "name": %s, "symbol": %s, "owner": %s, "issuers": %s, "max_count": %s}',
         to_jsonb(name)::text,
         to_jsonb(symbol)::text,
         to_jsonb(owner)::text,
@@ -34,11 +34,11 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE insert_nft_issue_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, holder hive.account_name_type, data jsonb, tags nfttracker_app.tags, soulbound bool)
+CREATE OR REPLACE PROCEDURE insert_nft_issue_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, holder hive.account_name_type, data jsonb, tags nfttracker_app.tags, soulbound bool, pos INT DEFAULT 0)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    CALL insert_custom_json_operation(block_num, auth, 'NFT', format('{"action": "issue", "symbol": %s, "holder": %s, "data": %s, "soulbound": %s, "tags": %s}',
+    CALL insert_custom_json_operation(block_num, pos, auth, 'NFT', format('{"action": "issue", "symbol": %s, "holder": %s, "data": %s, "soulbound": %s, "tags": %s}',
         to_jsonb(symbol)::text,
         to_jsonb(holder)::text,
         to_jsonb(data)::text,
@@ -48,11 +48,11 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE insert_nft_soulbind_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, id INT, soulbound bool)
+CREATE OR REPLACE PROCEDURE insert_nft_soulbind_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, id INT, soulbound bool, pos INT DEFAULT 0)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    CALL insert_custom_json_operation(block_num, auth, 'NFT', format('{"action": "soulbind", "symbol": %s, "id": %s, "soulbound": %s}',
+    CALL insert_custom_json_operation(block_num, pos, auth, 'NFT', format('{"action": "soulbind", "symbol": %s, "id": %s, "soulbound": %s}',
         to_jsonb(symbol)::text,
         to_jsonb(id)::text,
         to_jsonb(soulbound)::text
@@ -60,11 +60,11 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE insert_nft_set_data_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, id INT, data jsonb)
+CREATE OR REPLACE PROCEDURE insert_nft_set_data_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, id INT, data jsonb, pos INT DEFAULT 0)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    CALL insert_custom_json_operation(block_num, auth, 'NFT', format('{"action": "set_data", "symbol": %s, "id": %s, "data": %s}',
+    CALL insert_custom_json_operation(block_num, pos, auth, 'NFT', format('{"action": "set_data", "symbol": %s, "id": %s, "data": %s}',
         to_jsonb(symbol)::text,
         to_jsonb(id)::text,
         to_jsonb(data)::text
@@ -72,11 +72,11 @@ BEGIN
 END;
 $$;
 
-CREATE OR REPLACE PROCEDURE insert_nft_transfer_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, id INT, to_account hive.account_name_type)
+CREATE OR REPLACE PROCEDURE insert_nft_transfer_op(block_num INT, auth hive.account_name_type, symbol nfttracker_app.symbol, id INT, to_account hive.account_name_type, pos INT DEFAULT 0)
 LANGUAGE plpgsql
 AS $$
 BEGIN
-    CALL insert_custom_json_operation(block_num, auth, 'NFT', format('{"action": "transfer", "symbol": %s, "id": %s, "to": %s}',
+    CALL insert_custom_json_operation(block_num, pos, auth, 'NFT', format('{"action": "transfer", "symbol": %s, "id": %s, "to": %s}',
         to_jsonb(symbol)::text,
         to_jsonb(id)::text,
         to_jsonb(to_account)::text
