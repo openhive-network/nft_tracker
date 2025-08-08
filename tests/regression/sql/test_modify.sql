e90e64ae3f81fcf82b66e945accd2e8b4a11bae7
Krzysztof Leśniak <klesniak@syncad.com>
2025-08-08 13:11:14 +0200

    Test checking incrementing max count

--- tests/regression/sql/test_modify.sql
+++ tests/regression/sql/test_modify.sql
@@ -3,10 +3,11 @@
 -- Given
 CALL insert_nft_register_op(block_num=>1, auth=>'alice', symbol=>'alice/ABC', name=>'test', owner=>'alice', issuers=>ARRAY['alice'], max_count=>12);
 CALL insert_nft_modify_op(block_num=>2, auth=>'alice', name=>'test-mod', symbol=>'alice/ABC', owner=>'bob', issuers=>ARRAY['bob'], max_count=>11);
+CALL insert_nft_modify_op(block_num=>3, auth=>'bob', name=>'test-3', symbol=>'alice/ABC', owner=>'charlie', issuers=>ARRAY['charlie'], max_count=>12); -- Incrementing max_count should fail
 
 -- When
 CALL nfttracker_sync_blocks();
 
 -- Then
-SELECT creator, owner, symbol::TEXT, name::TEXT, max_count, issuers::TEXT[] FROM types_view;
-SELECT ALL(updated_at > created_at) FROM types_view;
+SELECT creator, owner, symbol, name, max_count, issuers FROM types_view;
+SELECT updated_at > created_at FROM types_view;
