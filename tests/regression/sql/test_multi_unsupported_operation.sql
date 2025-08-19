c553a19d7ad0ed934814c1ba070fb8c8a37ee52f
Krzysztof Leśniak <klesniak@syncad.com>
2025-08-19 14:15:11 +0200

    Test unsupported action in multiactions operation

---
+++ tests/regression/sql/test_multi_unsupported_operation.sql
@@ -0,0 +1,16 @@
+-- Invalid action which is part of multiaction operation doesn't stop processing of the actions that follow it.
+
+-- Given
+CALL insert_nft_ops(block_num=>1, auth=>'alice', ops=>ARRAY[
+  nft_register_op(symbol=>'alice/XYZ', name=>'test', owner=>'alice', issuers=>ARRAY['alice'], max_count=>12),
+  nft_issue_op(symbol=>'alice/XYZ', holder=>'alice', data=>'{}', tags=>ARRAY[]::nfttracker_app.tags, soulbound=>FALSE),
+  '{"action": "nonexistent_action"}',
+  nft_transfer_op(symbol=>'alice/XYZ', id=>1, to_account=>'charlie')
+]);
+
+-- When
+CALL nfttracker_sync_blocks();
+
+-- Then
+SELECT creator, owner, symbol, holder, data, tags, soulbound FROM instances_view;
+SELECT updated_at = created_at FROM instances_view;
